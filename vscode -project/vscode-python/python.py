3649c076798179f735bc12ca483ae51b20f0538f
Sw0rdsMan7 <2016319254@qq.com>
2021-10-09 15:28:43 +0800

    project

--- vscode -project/vscode-python/python.py
+++ vscode -project/vscode-python/python.py
@@ -1,5 +1,16 @@
-print("dasdsa")
-x=1
-y=2
-z=x*y
-print(z)
+x = eval(input())
+print(x, "divisible by 2 and 3? ", end=' ')
+if x%2 ==0 and x%3 ==0:
+    print("Ture")
+else:
+    print("False")
+print(x, "divisible by 2 or 3?", end=' ')
+if x%2 ==0 or x%3 ==0:
+    print("Ture")
+else:
+    print("False")
+print(x, "divisible by 2 or 3, but not both?", end=' ')
+if x%2 ==0 and x%3 != 0 or x%3 ==0 and x%2 != 0:
+    print("Ture")
+else:
+    print("False")
